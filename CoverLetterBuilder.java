2a7dc625c78f6855e57b3ca038616919a462a79a
Ben <i@auburn.edu>
2017-09-17 13:20:46 -0500

    Fix spacing issues

--- CoverLetterBuilder.java
+++ CoverLetterBuilder.java
@@ -7,33 +7,52 @@ public class CoverLetterBuilder {
 
 	public static String input_dir = "./Letters_Demo_Input/";
 	public static String output_dir = "./Letters_Demo_Output/";
+	public static String company_list_dir = "./Company_List_Folder/";
 
+	// TODO: Remove the below 2 globals, put in collect input methods
 	public static String current_letter = input_dir + "Pre_Graduation_Letter.txt";
+	public static String current_company_list = company_list_dir + "Example_List.csv";
 
 	public static void main(String[] args) {
 
-		String companyName = "companyName";
-		String jobTitle = "jobTitle";
+		// String companyName = "companyName";
+		// String jobTitle = "jobTitle";
+		//
+		// //System.out.printf("%s %s")
+		//
+		// Scanner in = new Scanner(System.in);
+		//
+		// System.out.print("companyName: ");
+		// companyName = in.nextLine();
+		//
+		// System.out.print("jobTitle: ");
+		// jobTitle = in.nextLine();
+		//
+		// write_a_letter(companyName, jobTitle);
 
-		//System.out.printf("%s %s")
-
-		Scanner in = new Scanner(System.in);
-
-		System.out.print("companyName: ");
-		companyName = in.nextLine();
-
-		System.out.print("jobTitle: ");
-		jobTitle = in.nextLine();
-
-		fin(companyName, jobTitle);
-
-
-
+		String[] possible_jobs = read_in_company_list();
+		for(String job : possible_jobs) {
+			write_a_letter(job);
+		}
 	}
 
-	// Program that calls everything together.
-	public static void fin(String companyName, String jobTitle) {
-		String[] text = readInFile();
+	// Program that calls everything together given a line of csv.
+	public static void write_a_letter(String company_and_job) {
+		String[] job_elements = company_and_job.split(",");
+		String[] text = read_in_letter();
+		text = categories(text, job_elements[0], job_elements[1]);
+		text = remove_excess_space(text);
+
+		for(String t : text) {
+			System.out.println(t);
+		}
+
+		write(text, job_elements[0]);
+	}
+
+	// Program that calls everything together given a company and position name.
+	public static void write_a_letter(String companyName, String jobTitle) {
+		String[] text = read_in_letter();
 		text = categories(text, companyName, jobTitle);
 		write(text, companyName);
 	}
@@ -61,8 +80,13 @@ public class CoverLetterBuilder {
 		} catch (IOException e) {
 			System.out.println("You are Exceptional!");
 		}
+	}
 
-
+	public static String[] remove_excess_space(String[] lines) {
+		for(int i = 0; i < lines.length; i++) {
+			lines[i] = lines[i].replaceAll("\\s+", " ");
+		}
+		return lines;
 	}
 
 	// Basic search for the category names. They cannot end a sentence as a '.' will mess things up
@@ -78,30 +102,63 @@ public class CoverLetterBuilder {
 		return text;
 	}
 
+	// Read in the list of companies and positions to make letters for
+	public static String[] read_in_company_list() {
+		String csvFile = current_company_list;
+    String line = "";
+
+		List<String> company_and_job = new ArrayList<String>();
+
+    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+	    while ((line = br.readLine()) != null) {
+        if (line.equals("Company name,Position name")) {
+					continue;
+				}
+				// Get count of the number of commas in a line
+				int comma_count = line.length() - line.replace(",", "").length();
+				if (comma_count != 1) {
+					System.out.printf("There should only be 1 comma in line: %s. This "
+						+ "program cant handle that, yet.\n", line);
+					continue;
+				}
+				company_and_job.add(line);
+			}
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+		// Switching from a list to an array.
+		String[] ret_arr = new String[company_and_job.size()];
+		ret_arr = company_and_job.toArray(ret_arr);
+
+		return ret_arr;
+	}
+
 	// Read in the file and return an array of the words.
-	public static String[] readInFile() {
+	public static String[] read_in_letter() {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br;
 		try {
-            br = new BufferedReader(new FileReader(current_letter));
-            try {
-                String x;
-                while ((x = br.readLine()) != null ) {
-                    // printing out each line in the file
-                    //System.out.println(x);
-                    // ret += x;
-										sb.append(x);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-            e.printStackTrace();
+      br = new BufferedReader(new FileReader(current_letter));
+      try {
+        String x;
+        while ((x = br.readLine()) != null ) {
+          // printing out each line in the file
+          //System.out.println(x);
+          // ret += x;
+					sb.append(x);
         }
-        String arr[] = sb.toString().split(" ");
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+    } catch (FileNotFoundException e) {
+        System.out.println(e);
+        e.printStackTrace();
+    }
+    String arr[] = sb.toString().split(" ");
 		return arr;
 	}
+
 
 
 
